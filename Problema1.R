a74252952a082b59a47df78ec375c123ebf53a50
MarioArriaza <mario.arriaza@estudiantat.upc.edu>
2025-03-04 09:46:56 +0100

    Update Problema1 marketplace.R

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,19 @@
 #Codigo para problema 1
+#Codigo problema 1 (MartketPlace)
+
+#a) Me preguntan el apartado R-squared
+Rsq <- 0.1923
+Rsq
+
+#b)
+R <- sqrt(Rsq)
+R
+
+#c) Me pregunta la pendiente (m)
+m <- 0.06576
+
+#d)
+b <- 11.79755
+b
+
+m*647.1+b #Respuesta
